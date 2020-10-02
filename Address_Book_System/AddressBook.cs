be0e15c1b5d6b30c72b7d07223ef117f7ecf201b
VSRUJANA <srujana.valavala@gmail.com>
2020-10-02 12:51:40 +0530

    [srujana] Add. Program to Add contact to address book is added

--- Address_Book_System/AddressBook.cs
+++ Address_Book_System/AddressBook.cs
@@ -4,14 +4,43 @@ using System.Text;
 
 namespace Address_Book_System
 {
-	public class AddressBook
-	{
-		public static List<Contact> contactList;
-		public AddressBook()
-		{
-			contactList = new List<Contact>();
-		}
-	}
+    public class AddressBook
+    {
+        public static List<Contact> contactList;
+        public AddressBook()
+        {
+            contactList = new List<Contact>();
+        }
+
+        //Function to add contact to Address book
+        public void AddContact(string[] d)
+        {
+            Contact entry = new Contact(d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7]);
+            contactList.Add(entry);
+            Console.WriteLine("Contact added successfully");
+        }
+
+        //Function to print contacts in Address book
+        public static void PrintContact()
+        {
+            if (AddressBook.contactList.Count != 0)
+            {
+                int count = 0;
+                foreach (Contact person in contactList)
+                {
+                    count += 1;
+                    Console.WriteLine("Contact- " + count);
+                    Console.WriteLine("Name      : " + person.firstName + " " + person.lastName);
+                    Console.WriteLine("Address   : " + person.address + ", " + person.city + ", " + person.state + "-" + person.zipCode);
+                    Console.WriteLine("Phone No. : " + person.phoneNumber);
+                    Console.WriteLine("Email id  : " + person.email);
+                    Console.WriteLine("-------------------------------------------------------------");
+                }
+            }
+            else
+            {
+                Console.WriteLine("There are no contacts in the address book!");
+            }
+        }
+    }
 }
-
-
--- Address_Book_System/Program.cs
+++ Address_Book_System/Program.cs
@@ -8,8 +8,29 @@ namespace Address_Book_System
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program!");
-            Console.WriteLine("Created Contact in Address Book...");
+            bool var = true;
+            var AB = new AddressBook();
+            while (var)
+            {
+                Console.WriteLine("Enter your choice :\n1.Add Contact\n2.Print Address Book\n3.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter details separated by comma in this format - First Name,Last Name,Address,City,State,ZipCode,Phone Number,Email");
+                        string[] details;
+                        details = Console.ReadLine().Split(",");
+                        AB.AddContact(details);
+                        break;
+                    case 2:
+                        AddressBook.PrintContact();
+                        break;
+                    default:
+                        var = false;
+                        Console.WriteLine("press any key to exit...");
+                        break;
+                }
+            }
         }
     }
 }
-
